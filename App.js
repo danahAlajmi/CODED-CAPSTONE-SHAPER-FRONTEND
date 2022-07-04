602a694c41730ca942b1ee8b54576d32bd973d9b
danahAlajmi <danah38x@gmail.com>
2022-07-04 20:43:29 +0300

    EDIT Prfoile

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Profile from "./components/profile/Profile";
+import EditProfile from "./components/profile/EditProfile";
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Text>hello danah</Text> */}
-      <Profile />
+      <EditProfile />
       <StatusBar style="auto" />
     </View>
   );
