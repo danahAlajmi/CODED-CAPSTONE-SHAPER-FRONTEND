e64e1d3617704a80cf9d99db8795207954366214
danahAlajmi <danah38x@gmail.com>
2022-07-05 18:18:06 +0300

    edit profile upload image

--- App.js
+++ App.js
@@ -2,13 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Profile from "./components/profile/Profile";
 import EditProfile from "./components/profile/EditProfile";
-<<<<<<< HEAD
-import ProfileUserView from "./components/profile/ProfileUserView";
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <ProfileUserView />
-=======
 import { SignInPage } from "./components/user/SignInPage";
 import { SignUpPage } from "./components/user/SignUpPage";
 import { CreateProfile } from "./components/user/CreateProfile";
@@ -17,15 +10,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <SignInPage />
->>>>>>> origin/main
       <StatusBar style="auto" />
     </View>
   );
 }
-
-
-
-
 
 const styles = StyleSheet.create({
   container: {
